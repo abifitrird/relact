af1b74ca46899322c4291a10a35e4e9bfc07acf3
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2020-03-17 17:48:37 +0700

    ubah folder sessions disimpan secara explicit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/relact/';
+$config['base_url'] = 'http://relact.test/';
 
 /*
 |--------------------------------------------------------------------------
@@ -380,7 +380,7 @@ $config['encryption_key'] = '?xx&2>u"#GT}sfQpw{G/x38jX?Z(uDEKm$^C=}F=n@X=`.f8^*c
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'relact_sess';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = "./sessions/";
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = TRUE;
