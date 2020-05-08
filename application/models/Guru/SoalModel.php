bb2adec6c8c8ab3f7ce3a27f3c54be6d087f90bf
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2020-05-09 06:18:26 +0700

    fix kunci jawaban on table; really fixed with esai section

--- application/models/Guru/SoalModel.php
+++ application/models/Guru/SoalModel.php
@@ -9,10 +9,10 @@ class SoalModel extends CI_Model
      */
     public function getSoalByKodeMateri($kode_materi)
     {
-        $data = $this->db->select('soal.id, soal. materi_kode, soal.tipe, soal.pertanyaan, soal.bobot, pilihan_soal.pilihan')
-        ->join('pilihan_soal', 'soal.id = pilihan_soal.soal_id', 'left')
-        ->join('kunci_soal', 'pilihan_soal.id = kunci_soal.pilihan_soal_id', 'left')
-        ->where('pilihan_soal.id = kunci_soal.pilihan_soal_id')
+        /* 
+        SELECT soal.id, soal.materi_kode, soal.tipe, soal.pertanyaan, soal.bobot, (SELECT pilihan_soal.pilihan FROM pilihan_soal JOIN kunci_soal ON pilihan_soal.id = kunci_soal.pilihan_soal_id WHERE soal.id = pilihan_soal.soal_id) as pilihan FROM soal
+        */
+        $data = $this->db->select('soal.id, soal. materi_kode, soal.tipe, soal.pertanyaan, soal.bobot, (SELECT pilihan_soal.pilihan FROM pilihan_soal JOIN kunci_soal ON pilihan_soal.id = kunci_soal.pilihan_soal_id WHERE soal.id = pilihan_soal.soal_id) as pilihan' )
         ->where('materi_kode', $kode_materi)
         ->get('soal');
         return $data->result_array();
