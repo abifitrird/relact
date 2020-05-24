4574a76faecb94c2027d18ac4453f23a698aa999
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2020-05-24 18:47:24 +0700

    fixed materi

--- application/views/guru/list_materi.php
+++ application/views/guru/list_materi.php
@@ -8,7 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/css/main.css') ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/summernote-bs4.min.css') ?>">
     <link rel="icon" href="<?php echo base_url('assets/images/logoTab_Relact.png') ?>">
     <script src="<?php echo base_url('assets/js/jquery-3.4.1.slim.min.js') ?>"></script>
     <script src="<?php echo base_url('assets/js/popper.min.js') ?>"></script>
@@ -37,53 +36,13 @@
 
 <!-- /#page-content-wrapper -->
 
-<div class="modal fade" id="tambahMateri" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Tambah Materi Baru</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="<?php echo site_url('guru/kelas/' . $this->uri->segment(3) . '/materi') ?>" method="POST">
-                    <div class="form-group">
-                        <label for="judul">Judul</label>
-                        <input type="text" class="form-control" id="judul" name="judul" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="editor">Konten</label>
-                        <textarea class="summernote form-control" id="summernote" name="konten" required></textarea>
-                    </div>
-
-            </div>
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-primary">Simpan</button>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-
 <!-- Menu Toggle Script -->
-<script src="<?php echo base_url('assets/js/summernote-bs4.min.js') ?>"></script>
 
 <script>
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
-
-    $(document).ready(function() {
-        $('#summernote').summernote({
-            dialogsInBody: true,
-        })
-        $('#summernote').each(function() {
-            $(this).val($(this).summernote('code'));
-        })
-    })
 </script>
 </body>
 
--- application/views/guru/materi.php
+++ application/views/guru/materi.php
@@ -8,7 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/css/main.css') ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/summernote-bs4.min.css') ?>">
     <link rel="icon" href="<?php echo base_url('assets/images/logoTab_Relact.png') ?>">
     <script src="<?php echo base_url('assets/js/jquery-3.4.1.slim.min.js') ?>"></script>
     <script src="<?php echo base_url('assets/js/popper.min.js') ?>"></script>
@@ -21,37 +20,6 @@
     <h1><?php echo $data['judul'] ?></h1>
     <div>
         <p><?php echo $data['konten'] ?></p>
-    </div>
-</div>
-
-
-<div class="modal fade" id="ubahMateri" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Ubah Materi</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="<?php echo site_url('guru/kelas/' . $this->uri->segment(3) . '/materi/ubah/' . $this->uri->segment(5)) ?>" method="POST" id="ubahMateriForm">
-                    <div class="form-group">
-                        <label for="judul">Judul</label>
-                        <input type="text" class="form-control" id="judul" name="judul" required value="<?php echo $data['judul'] ?>">
-                    </div>
-                    <div class="form-group">
-                        <label for="summernote">Konten</label>
-                        <textarea class="summernote form-control" id="summernote" name="konten" data-konten="<?php echo $data['konten'] ?>"></textarea>
-                    </div>
-
-            </div>
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-primary">Simpan</button>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                </form>
-            </div>
-        </div>
     </div>
 </div>
 
@@ -77,58 +45,16 @@
     </div>
 </div>
 
-<!-- footer -->
-<?php //include("footer.php") 
-?>
 <!-- /#page-content-wrapper -->
 </div>
 </div>
 
 <!-- Menu Toggle Script -->
-<script src="<?php echo base_url('assets/js/summernote-bs4.min.js') ?>"></script>
 <script>
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
-
-    $(document).ready(function() {
-        <<
-        <<
-        << < HEAD
-        const editor = $('#summernote').summernote({
-            dialogsInBody: true,
-            disableDragAndDrop: true,
-            spellcheck: false,
-            callbacks: {
-                onImageUpload: function(files) {
-                    if (!files.length) return;
-                    for (let i = 0; i < files.length; i++) {
-                        uploadImage(files[i])
-                    }
-                }
-            }
-        })
-        fetch('/api/materi/<?php echo $this->uri->segment(5) ?>').then(response => response.json())
-            .then(data => {
-                $('#summernote').summernote('code', data.konten);
-                $('#summernote').each(function() {
-                    $(this).val($(this).summernote('code'));
-                })
-            })
-
-        function uploadImage(file) {
-            let data = new FormData();
-            data.append('file', file);
-            fetch('/api/image', {
-                method: "POST",
-                body: data
-            }).then(response => response.json()).then(data => {
-                // console.log(data);
-                $('#summernote').summernote('insertImage', data.path);
-            });
-        }
-    })
 </script>
 </body>
 
