e2192b8765acc9d19cdb18ca277dbc34e5908e04
abifitrird <fratnadewi@gmail.com>
2020-05-27 21:10:06 +0700

    Update header.php
    ganti icon

--- application/views/guru/header.php
+++ application/views/guru/header.php
@@ -31,7 +31,7 @@
                 </a>
                 <a href="#" class="list-group-item list-group-item-action mb-2 bg-light">
                     <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M17 4V2H7V4H2V11C2 12.1 2.9 13 4 13H7.1C7.5 14.96 9.04 16.5 11 16.9V19.08C8 19.54 8 22 8 22H16C16 22 16 19.54 13 19.08V16.9C14.96 16.5 16.5 14.96 16.9 13H20C21.1 13 22 12.1 22 11V4H17M4 11V6H7V11L4 11M20 11L17 11V6H20L20 11Z" /></svg>
+                        <path fill="currentColor" d="M11 9C11 10.66 9.66 12 8 12C6.34 12 5 10.66 5 9C5 7.34 6.34 6 8 6C9.66 6 11 7.34 11 9M14 20H2V18C2 15.79 4.69 14 8 14C11.31 14 14 15.79 14 18M22 12V14H13V12M22 8V10H13V8M22 4V6H13V4Z" /></svg>
                     Aktivitas Siswa
                 </a>
                 <a href="<?php echo site_url("guru/saran") ?>" class="list-group-item list-group-item-action mb-2 bg-light">
