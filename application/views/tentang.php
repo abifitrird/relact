c642a03463d223a8f1d9653aba935c2764a164cf
abifitrird <fratnadewi@gmail.com>
2020-05-28 15:04:00 +0700

    edit style Tentang

--- application/views/tentang.php
+++ application/views/tentang.php
@@ -13,6 +13,7 @@
     <script src="<?php echo base_url('assets/js/jquery-3.4.1.slim.min.js') ?>"></script>
     <script src="<?php echo base_url('assets/js/popper.min.js') ?>"></script>
     <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+    <script src="<?php echo base_url('assets/js/about-us.js') ?>"></script>
     <style>
         body {
             font-family: "Times New Roman", Georgia, Serif;
