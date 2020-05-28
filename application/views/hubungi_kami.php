7c69cbd5261e7d87ce128fbaae8dd52378e3cc0c
abifitrird <fratnadewi@gmail.com>
2020-05-28 13:34:32 +0700

    add halaman Tentang

--- application/views/hubungi_kami.php
+++ application/views/hubungi_kami.php
@@ -8,30 +8,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/css/main.css') ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/contact_us.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/contact-us.css') ?>">
     <link rel="icon" href="<?php echo base_url('assets/images/logoTab_Relact.png') ?>">
     <script src="<?php echo base_url('assets/js/jquery-3.4.1.slim.min.js') ?>"></script>
     <script src="<?php echo base_url('assets/js/popper.min.js') ?>"></script>
     <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
-    <script src="<?php echo base_url('assets/js/contact_us.js') ?>"></script>
+    <script src="<?php echo base_url('assets/js/contact-us.js') ?>"></script>
 </head>
 <!-- navbar -->
 <nav class="navbar navbar-expand-lg border-bottom shadow-sm">
     <a href="<?php echo base_url() ?>">
         <h5 style="color: white">Recording Learning Activities</h5>
     </a>
-    <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
-        <li class="nav-item dropdown">
-            <a class="nav-item nav-link dropdown-toggle mr-md-2" href="#" id="bd-versions" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
-                <span class="text-white"><?php echo $this->session->userdata("nama"); ?></span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="bd-versions">
-                <a class="dropdown-item" href="#">Bantuan</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="<?php echo base_url('Login/Logout') ?>">Keluar</a>
-            </div>
-        </li>
-    </ul>
 </nav>
 
 <header>
---
+++ application/views/tentang.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Tentang</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/css/main.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/about-us.css') ?>">
+    <link rel="icon" href="<?php echo base_url('assets/images/logoTab_Relact.png') ?>">
+    <script src="<?php echo base_url('assets/js/jquery-3.4.1.slim.min.js') ?>"></script>
+    <script src="<?php echo base_url('assets/js/popper.min.js') ?>"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+    <style>
+        body {
+            font-family: "Times New Roman", Georgia, Serif;
+        }
+
+        h1,
+        h2 {
+            font-family: "Playfair Display";
+            letter-spacing: 5px;
+        }
+    </style>
+</head>
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg border-bottom shadow-sm">
+    <a href="<?php echo base_url() ?>">
+        <h5 style="color: white">Recording Learning Activities</h5>
+    </a>
+</nav>
+
+<!-- Page content -->
+<div class="w3-content" style="max-width:1100px">
+
+    <!-- About Section -->
+    <div class="w3-row w3-padding-64" id="about">
+        <div class="w3-col m6 w3-padding-large w3-hide-small">
+            <img src="<?php echo base_url('assets/images/logo_Relact.png') ?>" class="w3-round w3-image w3-opacity-med" alt="Table Setting" width="360" height="360">
+        </div>
+
+        <div class="w3-col m6 w3-padding-large">
+            <h1 class="w3-center">Tentang RELACT</h1><br>
+            <p class="w3-large">Recording Learning Activities (RELACT) adalah sebuah sistem manajemen pembelajaran yang merupakan gabungan dari teknologi media berbasis Cloud dengan metode Blended Learning.</p>
+            <p class="w3-large">Dengan adanya sistem manajemen pembelajaran ini memungkinkan siswa mengakses materi pembelajaran dari guru baik di dalam maupun di luar ruangan kelas. Selain itu, RELACT berperan sebagai pencatat aktivitas belajar siswa yang dapat membantu guru melakukan tugasnya dengan lebih mudah. Dengan menggunakan RELACT, belajar dapat dilakukan kapan saja dan di mana saja.</p>
+            <p class="w3-large">Tujuan dirancangnya sistem ini adalah untuk mendukung proses belajar siswa, menyediakan media belajar, dan membantu asistensi proses belajar siswa.</p>
+        </div>
+    </div>
+
+    <!-- End page content -->
+</div>
+
+
+
+<!-- footer -->
+<?php include("footer.php") ?>
