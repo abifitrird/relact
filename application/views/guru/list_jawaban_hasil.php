84325ed5d7396e229282833a4080a16c2c54be5d
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2020-06-14 23:37:42 +0700

    fix button to show sidebar on hasil kerja siswa

--- application/views/guru/list_jawaban_hasil.php
+++ application/views/guru/list_jawaban_hasil.php
@@ -58,6 +58,12 @@
             </div>
         </div>
     </div>
+    <script>
+        $("#menu-toggle").click(function(e) {
+            e.preventDefault();
+            $("#wrapper").toggleClass("toggled");
+        });
+    </script>
 </body>
 
 <?php foreach ($data as $dat) { ?>
