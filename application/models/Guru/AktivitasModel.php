d1c75ea46c594e2261ba5802442959efc17d686f
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2020-06-17 22:30:52 +0700

    fix aktivitas isi soal

--- application/models/Guru/AktivitasModel.php
+++ application/models/Guru/AktivitasModel.php
@@ -27,13 +27,7 @@ class AktivitasModel extends CI_Model
 
     private function getDataSoalMateri($kelas_kode, $user_id)
     {
-        return $this->db->query("SELECT materi.judul as judul_materi,
-(SELECT created_at FROM log_activity WHERE user_id = $user_id AND materi_id = materi.id AND is_soal IS NOT NULL ORDER BY created_at ASC LIMIT 1) as mulai,
-(SELECT created_at FROM log_activity WHERE user_id = $user_id AND materi_id = materi.id AND is_soal IS NOT NULL ORDER BY created_at DESC LIMIT 1) as akhir,
-TIMEDIFF((SELECT created_at FROM log_activity WHERE user_id = $user_id AND materi_id = materi.id AND is_soal IS NOT NULL ORDER BY created_at DESC LIMIT 1), (SELECT created_at FROM log_activity WHERE user_id = $user_id AND materi_id = materi.id AND is_soal IS NOT NULL ORDER BY created_at ASC LIMIT 1)) as durasi
-FROM materi
-WHERE kelas_id = (SELECT id FROM kelas WHERE kelas.code = '$kelas_kode' LIMIT 1)
-ORDER BY `created_at` DESC")->result_array();
+        return $this->db->query("SELECT id, url, kelas_id, (SELECT judul FROM materi WHERE materi.id = materi_id LIMIT 1) as judul_materi, (SELECT judul FROM sub_materi WHERE sub_materi.id = sub_materi_id LIMIT 1) as judul_submateri, created_at as mulai, (SELECT created_at FROM log_activity as log1 WHERE log1.user_id = log_master.user_id AND log1.is_soal IS NOT NULL AND TIMEDIFF(log1.created_at, log_master.created_at) > 0 ORDER BY TIMEDIFF(log1.created_at, log_master.created_at) DESC LIMIT 1) as akhir, TIMEDIFF((SELECT created_at FROM log_activity as log1 WHERE log1.user_id = log_master.user_id AND log1.is_soal IS NOT NULL AND TIMEDIFF(log1.created_at, log_master.created_at) > 0 ORDER BY TIMEDIFF(log1.created_at, log_master.created_at) DESC LIMIT 1), log_master.created_at) as durasi FROM `log_activity` as log_master WHERE user_id = $user_id AND kelas_id = (SELECT id FROM kelas WHERE kelas.code = '$kelas_kode' LIMIT 1) AND is_soal IS NOT NULL GROUP BY materi_id ORDER BY `created_at`  ASC")->result_array();
     }
 
     private function getDataSubMateri($kelas_kode, $user_id)
