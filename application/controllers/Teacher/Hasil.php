96569b66955a41a3455f54a73836c6b3825ebc60
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2020-06-25 10:09:10 +0700

    add breadcrumb to Hasil

--- application/controllers/Teacher/Hasil.php
+++ application/controllers/Teacher/Hasil.php
@@ -14,6 +14,8 @@ class Hasil extends Base
     public function viewHasilKerja()
     {
         $data['data'] = $this->Kelas->getKelas();
+        $this->breadcrumb->push('Guru', '/guru');
+        $this->breadcrumb->push('Hasil', '/guru/hasil');
         $this->load->view('guru/list_kelas_hasil', $data);
     }
 
@@ -21,13 +23,21 @@ class Hasil extends Base
     {
         $data['kelas'] = $this->Kelas->getKelasByKode($kode_kelas);
         $data['data'] = $this->Kelas->getMateriByKodeKelas($kode_kelas);
+        $this->breadcrumb->push('Guru', '/guru');
+        $this->breadcrumb->push('Hasil', '/guru/hasil');
+        $this->breadcrumb->push($data['kelas']['mapel'], '/guru/hasil/' . $kode_kelas);
         $this->load->view('guru/list_materi_hasil', $data);
     }
 
     public function viewHasilPerMateri($kode_kelas, $kode_materi)
     {
         $data['kelas'] = $this->Kelas->getKelasByKode($kode_kelas);
+        $materi = $this->Kelas->getMateriByKode($kode_materi);
         $data['data'] = $this->Hasil->getNilaiSiswaByMateri($kode_materi);
+        $this->breadcrumb->push('Guru', '/guru');
+        $this->breadcrumb->push('Hasil', '/guru/hasil');
+        $this->breadcrumb->push($data['kelas']['mapel'] . " / " . $materi['judul'], '/guru/hasil/' . $kode_kelas);
+        $this->breadcrumb->push("Daftar Siswa", "#");
         $this->load->view('guru/list_siswa_hasil', $data);
     }
 
@@ -38,6 +48,13 @@ class Hasil extends Base
         } else if ($mode == "Esai") {
             $data['data'] = $this->Hasil->getEsaiBySiswaId($user_id, $kode_materi);
         }
+        $materi = $this->Kelas->getMateriByKode($kode_materi);
+        $kode_kelas = $this->uri->segment(3);
+        $this->breadcrumb->push('Guru', '/guru');
+        $this->breadcrumb->push('Hasil', '/guru/hasil');
+        $this->breadcrumb->push($data['kelas']['mapel'] . " / " . $materi['judul'], '/guru/hasil/' . $kode_kelas);
+        $this->breadcrumb->push("Daftar Siswa", '/guru/hasil/' . $kode_kelas . '/materi/' . $kode_materi);
+        $this->breadcrumb->push("Periksa Jawaban", "#");
         $this->load->view('guru/list_jawaban_hasil', $data);
     }
 
