f93053ebb47df563f67fd9595c69a72c3cf4607d
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2020-06-25 10:29:00 +0700

    siswa/materi breadcrumb done

--- application/controllers/Student/Materi.php
+++ application/controllers/Student/Materi.php
@@ -10,11 +10,16 @@ class Materi extends Base
 	{
 		parent::__construct();
 		$this->load->model('Siswa/MateriModel', "Materi");
+		$this->load->model("KelasModel");
 	}
 
 	public function index($kode)
 	{
 		$data['data'] = $this->Materi->getMateriByKodeKelas($kode);
+		$this->breadcrumb->push('Siswa', '/siswa');
+		$this->breadcrumb->push('Kelas', '/siswa/kelas');
+		$this->breadcrumb->push($data['data'][0]['nama_mapel'], '/siswa/kelas/' . $kode);
+		$this->breadcrumb->push("Daftar Materi", '#');
 		$this->load->view('siswa/list_materi', $data);
 	}
 
@@ -29,6 +34,14 @@ class Materi extends Base
 		$user_id = $this->session->userdata('user_id');
 		$data['data'] = $this->Materi->getSubMateriByKodeMateri($materi_kode);
 		$data['status_soal'] = $this->Materi->checkNilaiByUserId($user_id);
+		$uri_kelas = $this->uri->uri_to_assoc(2);
+		$kelas = $this->KelasModel->getKelasByKode($uri_kelas['kelas']);
+		$materi = $this->KelasModel->getMateriByKode($materi_kode);
+		$this->breadcrumb->push('Siswa', '/siswa');
+		$this->breadcrumb->push('Kelas', '/siswa/kelas');
+		$this->breadcrumb->push($kelas['mapel'], '/siswa/kelas/' . $uri_kelas['kelas']);
+		$this->breadcrumb->push($materi['judul'], '/siswa/kelas/' . $uri_kelas['kelas'] . '/materi/' . $uri_kelas['materi']);
+		$this->breadcrumb->push("Daftar Submateri", '#');
 		$this->load->view('siswa/list_submateri', $data);
 	}
 
@@ -42,6 +55,14 @@ class Materi extends Base
 	public function showMateri($materi_kode, $sub_id)
 	{
 		$data['data'] = $this->Materi->getMateriByKodeMateri($materi_kode, $sub_id);
+		$uri_kelas = $this->uri->uri_to_assoc(2);
+		$kelas = $this->KelasModel->getKelasByKode($uri_kelas['kelas']);
+		$materi = $this->KelasModel->getMateriByKode($materi_kode);
+		$this->breadcrumb->push('Siswa', '/siswa');
+		$this->breadcrumb->push('Kelas', '/siswa/kelas');
+		$this->breadcrumb->push($kelas['mapel'], '/siswa/kelas/' . $uri_kelas['kelas']);
+		$this->breadcrumb->push($materi['judul'], '/siswa/kelas/' . $uri_kelas['kelas'] . '/materi/' . $uri_kelas['materi']);
+		$this->breadcrumb->push($data['data']['judul'], '#');
 		$this->load->view('siswa/materi', $data);
 	}
 }
